7ecdaf7c526a10c22ba05c66ce7526c38c5b4df2
Gökhan Eski <eskigkhn@gmail.com>
2025-12-19 23:53:19 +0300

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import { AuthProvider } from "../contexts/AuthContext";
 import ClientLayout from "../components/common/ClientLayout";
